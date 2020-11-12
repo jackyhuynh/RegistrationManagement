d7ae37afebb89ae2704c06bab281a7230d38580f
Truc Huynh <jackyhuynh87@gmail.com>
2020-11-12 13:54:23 -0500

    Update BST.cpp

--- BST.cpp
+++ BST.cpp
@@ -2,8 +2,8 @@
 //  BST.cpp
 //  Final_project
 //
-//  Created by YIEN LIN on 6/19/14.
-//  Copyright (c) 2014 YIEN LIN. All rights reserved.
+//  Created by YIEN LIN, TRUC HUYNH on 6/19/14.
+//  Copyright (c) 2014 YIEN LIN, TRUC HUYNH. All rights reserved.
 //
 
 #include "BST.h"
